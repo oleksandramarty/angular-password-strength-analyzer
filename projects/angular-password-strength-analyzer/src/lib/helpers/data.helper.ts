ae6c14a117f0fa447b9e364c4ca3a8490a4aea1c
Alexander <amarty777@gmail.com>
2023-04-16 19:26:33 +0300

    update readme fix test

--- projects/angular-password-strength-analyzer/src/lib/helpers/data.helper.ts
+++ projects/angular-password-strength-analyzer/src/lib/helpers/data.helper.ts
@@ -1,5 +1,6 @@
 export const passwordWeightDefaultText = {
   empty: 'empty',
+  tooShort: 'too short',
   weak: 'Weak',
   good: 'Good',
   excellent: 'Excellent',
@@ -7,7 +8,8 @@ export const passwordWeightDefaultText = {
 
 export const defaultConfig = [
   { max: 0, text: passwordWeightDefaultText.empty },
-  { min: 1, max: 34, text: passwordWeightDefaultText.weak },
+  { min: 1, max: 1, text: passwordWeightDefaultText.tooShort },
+  { min: 2, max: 34, text: passwordWeightDefaultText.weak },
   { min: 35, max: 67, text: passwordWeightDefaultText.good },
   { min: 68, text: passwordWeightDefaultText.excellent },
 ]
